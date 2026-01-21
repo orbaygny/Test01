c115bd6b0dbb00c90d8b21026386f2a2ee9dd389
orbaygny <orbay_1997@hotmail.com>
2026-01-22 02:40:23 +0300

    Final

--- Assets/_Project/Scripts/Target.cs
+++ Assets/_Project/Scripts/Target.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.UI;
 public class Target : MonoBehaviour
 {
     [SerializeField] float _hp;
     [SerializeField] SpriteRenderer _spriteRenderer;
+    [SerializeField] Image _fill;
     public void TakeDamage(float damage)
     {
         _hp -= damage;
+        _fill.fillAmount = _hp / 10f;
         if (_hp <= 0)
         {
             _spriteRenderer.transform.parent = null;
@@ -19,6 +22,7 @@ public class Target : MonoBehaviour
     void RePosition()
     {
         _spriteRenderer.transform.position = transform.position - Vector3.right*-2;
+        _fill.fillAmount = 1f;
         _spriteRenderer.enabled = true;
         Sequence seq = DOTween.Sequence();
         seq.Append(_spriteRenderer.transform.DOMoveX(transform.position.x, 1f).SetEase(Ease.Linear)).
